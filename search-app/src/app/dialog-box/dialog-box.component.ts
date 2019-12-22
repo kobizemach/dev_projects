19a3d9a4d5a1a1de1862c659feba441fe7ed31b4
kobizemach <52497718+kobizemach@users.noreply.github.com>
2019-12-22 15:27:11 +0200

    Update dialog-box.component.ts

--- search-app/src/app/dialog-box/dialog-box.component.ts
+++ search-app/src/app/dialog-box/dialog-box.component.ts
@@ -3,8 +3,7 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 
 @Component({
   selector: 'app-dialog-box',
-  templateUrl: './dialog-box.component.html',
-  styleUrls: ['./dialog-box.component.css']
+  templateUrl: './dialog-box.component.html'
 })
 export class DialogBoxComponent  {
 
